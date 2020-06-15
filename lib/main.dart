5315f6a5b6f567ef340c7b373bdbecc96758f6cb
tanishksaraswat <tanishk.saraswat@gmail.com>
2020-06-15 14:38:26 +0530

    updated the debugBanner

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Calculator',
       theme: ThemeData(
         primarySwatch: Colors.yellow,
